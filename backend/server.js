3be7d53ff161a40b7581f8be49010e07cf58b518
aditya-varma8019 <rvenkataditya@gmail.com>
2024-03-25 14:28:26 +0530

    a

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import { dbConnect } from './configs/database.config.js';
+import cors from 'cors';
+// import userRouter from './src/routers/user.router.js';
+// import eventRouter from './src/routers/event.router.js';
+// import uploadRouter from './src/routers/upload.router.js';
+// import vendorRoute from './src/routers/vendor.router.js';
+
+dotenv.config();
+
+dbConnect();
+
+const app = express();
+app.use(express.json());
+app.use(cors()); // <-- Call the cors middleware function
+
+app.get("/", (req, res) => {
+    console.log("Server is running");
+    res.send("Server is running");
+});
+
+app.use("/api/users", userRouter);
+
+const port = 5000;
+
+app.listen(port, () => {
+    console.log(`Server started at http://localhost:` + port);
+});
